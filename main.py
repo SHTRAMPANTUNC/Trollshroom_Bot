22e1b3573dc37d54743b0cb077854237e1debcaf
SHTRAMPANTUNC <74539640+SHTRAMPANTUNC@users.noreply.github.com>
2022-04-22 15:52:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import telebot
-from youtube_dl import YoutubeDL
+from  youtube_dl import YoutubeDL
 import os.path
+from telebot import types
 
 
 bot = telebot.TeleBot('')
@@ -8,54 +9,67 @@ t = ''
 tu = ''
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands = ['start'])
 def start(message):
-    bot.send_message(message.chat.id, 'Привет! Введи ссылку на ролик')
-
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn = types.KeyboardButton("/start")
+    markup.add(btn)
+    bot.send_message(message.chat.id, 'Привет! Введи ссылку на ролик', reply_markup=markup)
 
 @bot.message_handler(content_types=['text'])
 def URL(message):
-    global t
-    global tu
 
-    if message.text.lower() == 'mp3' or message.text.lower() == 'mp4':
-        t = message.text.lower()
+  global t 
+  global tu
+  
+  if message.text.lower() == 'mp3' or  message.text.lower() == 'mp4':
+    t = message.text.lower()
 
-    if 'you' in message.text or 'you' in tu:
-        tu = message.text if 'you' in message.text else tu
-        if t == '':
-            bot.send_message(message.chat.id, 'Ведите тип: mp3\mp4 ')
+  
+  if 'you' in message.text or 'you' in tu:
+    tu = message.text if 'you' in message.text else tu
+    if t == '':
+      markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+      btn = types.KeyboardButton("mp3")
+      btn1 = types.KeyboardButton("mp4")
+      markup.add(btn, btn1)
+      bot.send_message(message.chat.id, 'Ведите тип: mp3\mp4 ', reply_markup=markup )
 
-            return
+      return
 
-        if t == 'mp3':
-            bot.send_message(message.chat.id, 'Подождите, идет загрузка')
-            mp3(message.chat.id, tu)
-            bot.send_audio(message.chat.id, open(str(message.chat.id) + '.' + t, 'rb'))
+    if t == 'mp3':
+      bot.send_message(message.chat.id, 'Подождите, идет загрузка')
+      mp3(message.chat.id, tu)
+      bot.send_audio(message.chat.id, open(str(message.chat.id) + '.' + t, 'rb'))
 
-        elif t == 'mp4':
-            bot.send_message(message.chat.id, 'Подождите, идет загрузка')
-            mp4(message.chat.id, tu)
-            bot.send_video(message.chat.id, open(str(message.chat.id) + '.' + t, 'rb'))
-
-        tu = ''
-        t = ''
-
-
-def mp3(id, url):
-    if os.path.exists(str(id) + '.' + t):
-        os.remove(str(id) + '.' + t)
-
-    audio_downloader = YoutubeDL({'format': 'bestaudio', 'outtmpl': str(id) + '.' + t})
-    audio_downloader.extract_info(url)
+    elif t == 'mp4':
+      bot.send_message(message.chat.id, 'Подождите, идет загрузка')
+      mp4(message.chat.id, tu)
+      bot.send_video(message.chat.id, open(str(message.chat.id) + '.' + t, 'rb'))
 
 
-def mp4(id, url):
-    if os.path.exists(str(id) + '.' + t):
-        os.remove(str(id) + '.' + t)
+    tu  = ''
+    t = ''
 
-    audio_downloader = YoutubeDL({'format': 'bestvideo', 'outtmpl': str(id) + '.' + t})
-    audio_downloader.extract_info(url)
+def mp3(id,url):
+
+  if os.path.exists(str(id) + '.' + t):
+    os.remove(str(id) + '.' + t)
+
+  audio_downloader = YoutubeDL({'format':'bestaudio', 'outtmpl' : str(id) + '.' + t})
+  audio_downloader.extract_info(url)
+
+
+def mp4(id,url):
+
+  if os.path.exists(str(id) + '.' + t):
+    os.remove(str(id) + '.' + t)
+
+  audio_downloader = YoutubeDL({'format':'bestvideo', 'outtmpl' : str(id) + '.' + t})
+  audio_downloader.extract_info(url)
+
+
 
 
 bot.polling()
+
